b581039e87ae37f7fe48f014007f7c4907d03606
gushiyu <923686643@qq.com>
2018-12-04 14:47:57 +0800

    修改啊啊啊

--- src/main/java/com/example/demo02/TestController1.java
+++ src/main/java/com/example/demo02/TestController1.java
@@ -1,13 +1,16 @@
 package com.example.demo02;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController1 {
-	
-	@RequestMapping("/hehe")
+
+    @Value("${phone}")
+	private String phone;
+	@RequestMapping("/hehehe")
 	public String hello(){
-		return "hello springboot";
+		return "hello springboot" + "&nbsp" + phone;
 	}
 }
---
+++ src/main/java/com/example/demo02/bean/Person.java
@@ -0,0 +1,65 @@
+package com.example.demo02.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
+import java.util.Map;
+@ConfigurationProperties(prefix = "person")
+@Component
+@Validated
+public class Person {
+     @Email
+     String name;
+     @Max(80)
+     @Min(18)
+     int age;
+     List<Object> hobby;
+     Map<String,String> friends;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Object> getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(List<Object> hobby) {
+        this.hobby = hobby;
+    }
+
+    public Map<String, String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Map<String, String> friends) {
+        this.friends = friends;
+    }
+
+    @Override
+    public String toString() {
+        return "persion{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", hobby=" + hobby +
+                ", friends=" + friends +
+                '}';
+    }
+}
